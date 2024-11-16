7d6f423a062904c8803b771bf198c2f0ed4f9ed5
OmarTelles114 <omar.telles@uabc.edu.mx>
2024-11-15 19:25:02 -0800

    FichaTridomino.java

--- FichaTridomino.java
+++ FichaTridomino.java
@@ -6,19 +6,23 @@ public class FichaTridomino extends Ficha {
         this.lado3 = lado3;
     }
 
-    @Override
-    public int getPuntos() {
-        return super.getPuntos() + lado3;
+    public int getLado3() {
+        return lado3;
     }
 
     @Override
-    public boolean puedeColocarse(Ficha otra) {
-        if (otra instanceof FichaTridomino) {
-            FichaTridomino fichaTridomino = (FichaTridomino) otra;
-            return lado1 == fichaTridomino.lado1 || lado1 == fichaTridomino.lado2 || lado1 == fichaTridomino.lado3 ||
-                    lado2 == fichaTridomino.lado1 || lado2 == fichaTridomino.lado2 || lado2 == fichaTridomino.lado3 ||
-                    lado3 == fichaTridomino.lado1 || lado3 == fichaTridomino.lado2 || lado3 == fichaTridomino.lado3;
+    public void ajustarParaConexion(int numeroConectar, boolean esVertical) {
+        while (lado1 != numeroConectar && lado2 != numeroConectar && lado3 != numeroConectar) {
+            rotateRight();
         }
+
+        if (esVertical && lado3 != numeroConectar) {
+            rotateRight(); // Asegura que lado3 sea el lado de conexión para vertical.
+        }
+    }
+
+    @Override
+    public boolean puedeConectarse(Ficha otra) {
         return lado1 == otra.lado1 || lado1 == otra.lado2 || lado2 == otra.lado1 || lado2 == otra.lado2 ||
                 lado3 == otra.lado1 || lado3 == otra.lado2;
     }
@@ -33,14 +37,15 @@ public class FichaTridomino extends Ficha {
 
     @Override
     public void rotateLeft() {
-        int temp = lado1;
-        lado1 = lado2;
-        lado2 = lado3;
-        lado3 = temp;
+        rotateRight(); // La rotación izquierda es equivalente aquí.
     }
 
     @Override
     public String toString() {
-        return "   *\n    * " + lado1 + " *\n  * " + lado2 + " | " + lado3 + " *\n*  *  *  *  *";
+        return "[ " + lado1 + " ]\n[ " + lado2 + " | " + lado3 + " ]";
+    }
+
+    public String toHorizontalString() {
+        return "[ " + lado2 + " | " + lado3 + " ]\n[ " + lado1 + " ]";
     }
 }
